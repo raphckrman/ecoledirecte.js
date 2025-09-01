741e551343e858d42486091b6551cd428fa6dc8a
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-09-02 00:26:45 +0200

    feat(school): add school structure

---
+++ src/structures/School.ts
@@ -0,0 +1,10 @@
+export class School {
+  constructor(
+    readonly id: string,
+    readonly ogec: string,
+    readonly name: string,
+    readonly logo: string,
+    readonly rne: string,
+    readonly phoneNumber: string,
+  ) {}
+}
